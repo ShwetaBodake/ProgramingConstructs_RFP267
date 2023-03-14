47966b502d5c77a95da369633892533ebe2723c8
Shweta Bodake <shwetabodake8108@gmail.com>
2023-03-14 07:34:24 +0530

    [Shweta] Even Or odd number is displayed

---
+++ ProgramingConstructs_RFP267/EvenOddNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ProgramingConstructs_RFP267
+{
+	public class EvenOddNumbers
+	{
+		public static void EvenOddNumbersDisplay()
+		{
+			Console.WriteLine("Eneter the number");
+			int num = Convert.ToInt32(Console.ReadLine());
+			if(num % 2==0)
+			{
+				Console.WriteLine("The givin number is EVEN");
+			}
+			else
+			{
+				Console.WriteLine("The givin number is ODD");
+			}
+		}
+	}
+}
+
--- ProgramingConstructs_RFP267/Program.cs
+++ ProgramingConstructs_RFP267/Program.cs
@@ -2,14 +2,26 @@
 //Console.WriteLine("Hello, World!");
 namespace ProgramingConstructs_RFP267
 {
-public class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("----------Welcome to Programing Constructs------------");
-            EqualityCheckOfInteger.EqualityOfInteger();
-            Console.ReadLine();
+            Console.WriteLine("Choose program from givin Option 1:Check numbers are equal are not\n 2:Display Even or Odd number");
+
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    EqualityCheckOfInteger.EqualityOfInteger();
+                    break;
+                case 2:
+                    EvenOddNumbers.EvenOddNumbersDisplay();
+                    break;
+                default:
+                    Console.WriteLine("Enter number within givin option");
+                    break;
+            }
         }
     }
 }
-
