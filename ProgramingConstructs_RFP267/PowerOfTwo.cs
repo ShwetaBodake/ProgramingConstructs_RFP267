5055d756f1ec94d1077c577c0159f9e5ede40486
Shweta Bodake <shwetabodake8108@gmail.com>
2023-03-14 22:41:53 +0530

    [Shweta] Power of table of two is displayed

---
+++ ProgramingConstructs_RFP267/PowerOfTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ProgramingConstructs_RFP267
+{
+	public class PowerOfTwo
+	{
+		public static void PowerOfTwoDisplay()
+		{
+			Console.WriteLine("Enter the number n");
+			int num = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("The table of power of two is:");
+			for(int i=0;i<=num;i++)
+			{
+				double power = Math.Pow(2,i);
+				Console.WriteLine(power);
+			}
+		}
+	}
+}
+
