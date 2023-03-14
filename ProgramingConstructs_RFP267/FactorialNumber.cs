42f6c3160f0d81d3217ffbf004f59eb8140fcc5c
Shweta Bodake <shwetabodake8108@gmail.com>
2023-03-14 23:07:41 +0530

    [Shweta] Factrial of givin number is found

---
+++ ProgramingConstructs_RFP267/FactorialNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ProgramingConstructs_RFP267
+{
+	public class FactorialNumber
+	{
+		public static void FactorialNumberdisplay()
+		{
+			int fact = 1;
+			Console.WriteLine("Enter the number");
+			int num = Convert.ToInt32(Console.ReadLine());
+			for(int i=1;i<=num;i++)
+			{
+				fact = fact * i;
+			}
+            Console.WriteLine("The factorial of givin number is {0}", fact);
+        }
+	}
+}
+
