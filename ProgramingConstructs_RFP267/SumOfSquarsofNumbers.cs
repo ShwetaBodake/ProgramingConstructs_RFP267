81a786227bbcba493b5fbdabdd81be9c9864d684
Shweta Bodake <shwetabodake8108@gmail.com>
2023-03-14 22:56:35 +0530

    [Shweta] Sum squars of number givin is calculated

---
+++ ProgramingConstructs_RFP267/SumOfSquarsofNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ProgramingConstructs_RFP267
+{
+	public class SumOfSquarsofNumbers
+	{
+		public static void SumOfSquarsofNumbersDisplay()
+		{
+			Console.WriteLine("Enter the  number");
+			int num = Convert.ToInt32(Console.ReadLine());
+			double totalsum = 0;
+			for(int i=1;i<=num;i++)
+			{
+				double sum = Math.Pow(i, 2);
+				totalsum += sum;
+			}
+			Console.WriteLine("Total sum of squars of numbers is:{0}", totalsum);
+		}
+	}
+}
+
