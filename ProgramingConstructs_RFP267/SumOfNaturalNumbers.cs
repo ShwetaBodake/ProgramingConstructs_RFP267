6dcb9781ac60377e8a3dacf81689bf8be0bdd542
Shweta Bodake <shwetabodake8108@gmail.com>
2023-03-16 17:39:03 +0530

    [Shweta] Sum of natural numbers are calculated

---
+++ ProgramingConstructs_RFP267/SumOfNaturalNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ProgramingConstructs_RFP267
+{
+	public class SumOfNaturalNumbers
+	{
+		public static void DieplaySumOfNaturalNumbers()
+		{
+            Console.WriteLine("The Sum of first 5 natural numbers ");
+            int start = 1;
+            int count = 1;
+            int sum = 0;
+            while (count <= 5)
+            {
+                sum = sum + start;
+                count++;
+                start++;
+            }
+            Console.WriteLine("Sum first 5 natural number is: " + sum);
+        }
+	}
+}
+
